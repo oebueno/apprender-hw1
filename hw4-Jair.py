683af9453fa6aedc3c552c1a4a34a6c01846ada3
Jair Herrera <unifly.jair@gmal.com>
2022-09-10 12:09:01 -0500

    I created a list of my favorite universities and i worked on a for loop

---
+++ hw4-Jair.py
@@ -0,0 +1,12 @@
+
+
+
+myUniversities = [ "ute", "uce", "utn" ]
+#print (myUniversities)
+chooseprorty = 1
+
+for university in myUniversities:
+    #print (chooseprorty)
+    #print (university)
+    print ("This is my " + str (chooseprorty) + " choise: " + university)
+    chooseprorty +=1
