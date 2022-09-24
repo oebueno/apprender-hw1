51a388c1374919b81aaf8b0845ecb334e374cc56
Jair Herrera <unifly.jair@gmal.com>
2022-09-24 12:04:03 -0500

    He realizado mas practicas con el for loop

--- hw4-Jair.py
+++ hw4-Jair.py
@@ -2,13 +2,29 @@
 
 
 myUniversities = [ "ute", "uce", "utn" ]
+publicUniversities = [ "uce", "ups", "ug" ]
+privateUniversities = [ "usfq", "utpl" ]
+universityTypes = {"public": publicUniversities, "private": privateUniversities}
+# print (publicUniversities [1])
+# print (universityTypes ["private"])
+# print (universityTypes["public"])
+#print (universityTypes)
 #print (myUniversities)
 chooseprorty = 1
 
-for university in myUniversities:
-    #print (chooseprorty)
-    #print (university)
-    print ("This is my " + str (chooseprorty) + " choise: " + university)
-    chooseprorty +=1
-    for letters in university:
-        print (letters)
+# for university in myUniversities:
+#     #print (chooseprorty)
+#     #print (university)
+#     print ("This is my " + str (chooseprorty) + " choise: " + university)
+#     chooseprorty +=1
+#     for letters in university:
+#         print (letters)
+
+for universitiesByType in universityTypes:
+    print (universitiesByType)#1.-public 2.- private
+    for universities in universityTypes[universitiesByType]:#valor de public university list, private list
+        print (universities)#1.- uce 2.- usfq
+# for universities in universityTypes["private"]:
+#     print (universities)
+# print (privateUniversities[0])
+# print (universityTypes["private"][1])
