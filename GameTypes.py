f2524f18fc419df913711c88fb9ff3f71338f7f3
Nick Yepez <nickyepez2104@gmail.com>
2022-10-08 13:37:57 -0500

    I Created a letter parameter

--- GameTypes.py
+++ GameTypes.py
@@ -1,22 +1,67 @@
-from operator import le
+#from operator import le
+#python from turtle import clear
 
+#fps = ["Call of duty", "Apex Legends", "Battlefield", "Valorant", "TitanFall"]
+#sandBox = ["Minecraft", "Gmod", "Terraria"]
+#br = ["Fortnite", "PUBG"]
+#survivalHorror = ["Resident Evil", "Silent Hill", "The Evil Within", "Dead Space"]
+#oba = ["LoL", "Smite"]
 
-fps = ["Call of duty", "Apex Legends", "Battlefield", "Valorant", "TitanFall"]
-sandBox = ["Minecraft", "Gmod", "Terraria"]
-br = ["Fortnite", "PUBG"]
-survivalHorror = ["Resident Evil", "Silent Hill", "The Evil Within", "Dead Space"]
-moba = ["LoL", "Smite"]
+#gameTypes = { "Fps": fps, "Sand Box": sandBox, "Br": br, "Survival Horror": survivalHorror, "Moba": moba }
 
-gameTypes = [ fps, sandBox, br, survivalHorror, moba ]
+#mainCharacters = ["Soap Mactavish", "Mirage", "Jonathan Miller", "Jett", "Ash", "Steve", "Jaro", "Steampunker", "Tfue", "DrDisrespect", "Jill Valentine", "Harry", "Sebastian", "Isaac Clarke", "Ahri", "Susano"]
 
-#print (gameTypes)
+#print (gameTypes["Moba"])
 
-for gameType in gameTypes:
+#for gameType in gameTypes:
     #print (gameType)
-    for game in gameType:
-        #print (game)
-        for letters in game:
-            #print (letters)
-            for i in letters:
-                print (i)
+    #print (gameTypes[gameType])
+    # print (gameType.values())
+#for game in gameType:
+     #print (game)
+    #     for letters in game:
+    #         print (letters)
 
+# firstName = "Nick"
+# lastName = "Yepez"
+
+# print(firstName + " " + lastName)
+
+#from re import S
+
+
+# s = "S"
+# s += "S"
+# s += "S"
+# s += "S"
+# print (s)
+# s = s[:-1]
+#for a in range (1,4):
+    #print (a)
+    #print (s)
+    # print (s)
+    # print(s[:-1])
+    #print(s[:-2])
+
+# s= "S"
+# s += "S"
+# s += "S"
+# s += "S"
+# #print (s)
+# for a in range (1,4):
+#     print (s[:a])
+# for a in range (1,4):
+#     print (s[1:-a])
+
+d = "E"
+s = d
+n = 322
+for i in range (0,n):
+    if (i == 0):
+        print (s)
+    else :
+        s += d
+        print (s)
+for i in range (1,n):
+    s=s[:-1]
+    print (s)
