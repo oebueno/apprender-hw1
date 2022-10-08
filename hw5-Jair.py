a2b0a356c3f9533488fc6b4cafa3b9bf58cc9570
Jair Herrera <unifly.jair@gmal.com>
2022-10-08 13:27:40 -0500

    we learn about basic data structures and string manipulation

---
+++ hw5-Jair.py
@@ -0,0 +1,46 @@
+# s = "Jair"
+# lastNamme = "Herrera"
+# s = "S"
+# s += "S"
+# s += "S"
+# s += "S"
+# print (s)
+# print (s[:-1])
+# print (s)
+# s = s[:-1]
+# print (s)
+# day = "Nick is having a great day"
+# print (day[:-10])
+
+# Code snippet
+# str[:-1]
+# Here, the str can be any string whose last character you want to remove.
+
+# data = "Dogecoin"
+# print(data)
+# Output
+# Dogecoi
+
+# print (s)
+
+#print (firstName + " " + lastNamme)
+#print ("Jair" + " " + "Herrera")
+# for i in range (1,4):
+    # print (i)
+    # print (s)
+# Pleas print the following structure using for loops
+# S
+# SS
+# SSS
+# SS
+# S
+
+s = "S"
+s += "S"
+s += "S"
+s += "S"
+# print (s)
+for i in range (1,4):
+    print (s[0:i])
+for i in range (1,4):
+    print (s[1:-i])
